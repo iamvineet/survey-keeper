0de30bc6583722ec63b69a2312c3f0c3b0532751
iamvineet <vineetpillai17@gmail.com>
2020-08-09 13:44:01 +0530

    final change

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import SurveyList from "./surveys/SurveyList";
 
 export default function Dashboard() {
     return (
       <div>
-        Dashboard new
+        <SurveyList />
         <div className="fixed-action-btn">
           <Link to="/surveys/new" className="btn-floating btn-large red">
             <i className="material-icons">add</i>
--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function SurveyField({ input, label, meta : { error, touched } }) {
     return (
